e90efb233b749d7bf17fa6578cc927a16276b633
AHAHACUK <mrtiwesyes@gmail.com>
2022-10-31 09:47:31 +0500

    Card Transition

--- lib/ui/card_list.dart
+++ lib/ui/card_list.dart
@@ -2,22 +2,32 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/models/character.dart';
 import 'package:rick_and_morty/ui/card_page.dart';
+import 'package:rick_and_morty/ui/circle_clipper.dart';
 
 class CharacterCardList extends StatelessWidget {
   final List<Character> characters;
-  const CharacterCardList(this.characters, { Key? key }) : super(key: key);
+  const CharacterCardList(this.characters, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: characters.length,
-      itemBuilder: (context, index) {
-        Character character = characters[index];
-        return ListTile(
-          title: Text(character.name),
-          onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => CharacterCardPage(character))),
-        );
-      }
-    );
+        itemCount: characters.length,
+        itemBuilder: (context, index) {
+          Character character = characters[index];
+          return ListTile(
+            leading: Hero(
+              tag: "character_avatar ${character.name}",
+              child: SizedBox(
+                width: 60,
+                child: ClipOval(
+                    clipper: CircleClipper(),
+                    child: Image.network(character.imgUrl)),
+              ),
+            ),
+            title: Text(character.name),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => CharacterCardPage(character))),
+          );
+        });
   }
 }
--- lib/ui/card_page.dart
+++ lib/ui/card_page.dart
@@ -11,75 +11,87 @@ import 'package:rick_and_morty/ui/circle_clipper.dart';
 class CharacterCardPage extends StatelessWidget {
   final Character character;
   static const TextStyle nameStyle = TextStyle(fontSize: 32);
-  static const TextStyle episodesHeaderStyle = TextStyle(fontSize: 20, color: Color.fromARGB(255, 87, 87, 87));
+  static const TextStyle episodesHeaderStyle =
+      TextStyle(fontSize: 20, color: Color.fromARGB(255, 87, 87, 87));
   const CharacterCardPage(this.character, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     EpisodeFetcher episodeFetcher = EpisodeFetcher();
-    FunctionLoaderBloc<List<Episode>> contentLoader = FunctionLoaderBloc(
+    FunctionLoaderBloc<List<Episode>> episodesLoader = FunctionLoaderBloc(
         () => episodeFetcher.fetchListByIds(character.episodesIds));
-    contentLoader.add(ContentLoadInit());
+    episodesLoader.add(ContentLoadInit());
     return Scaffold(
         appBar: AppBar(),
-        body: BlocBuilder<ContentLoaderBloc, ContentLoaderState>(
-          bloc: contentLoader,
-          builder: (context, state) {
-            if (state is NoContent) {
-              return Container();
-            }
-            if (state is LoadingError) {
-              return const Text('Woops :(');
-            }
-            if (state is ContentLoaded) {
-              List<Episode> episodes = state.content;
-              return Column(children: [
-                const SizedBox(height: 10),
-                Expanded(
-                    flex: 1,
-                    child: ClipOval(
-                        clipper: CircleClipper(),
-                        child: Image.network(character.imgUrl))),
-                Expanded(
-                    flex: 3,
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 4),
-                        child: Center(
-                            child: SizedBox(
-                          width: 500,
-                          child: Column(children: [
-                            Expanded(
-                                flex: 1,
-                                child: Text(character.name, style: nameStyle)),
-                            Expanded(
-                                flex: 4,
-                                child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 24),
-                                    child: Column(children: [
-                                      _FieldRow("Species", character.species),
-                                      _FieldRow("Type", (character.type.isNotEmpty ? character.type : "not specified")),
-                                      _FieldRow("Origin", character.origin),
-                                      const Text("Episodes", style: episodesHeaderStyle),
-                                      const SizedBox(height: 10),
-                                      Expanded(child: ListView.builder(
+        body: Column(children: [
+          const SizedBox(height: 10),
+          Expanded(
+              flex: 1,
+              child: Hero(
+                tag: "character_avatar ${character.name}",
+                child: ClipOval(
+                    clipper: CircleClipper(),
+                    child: Image.network(character.imgUrl)),
+              )),
+          Expanded(
+              flex: 3,
+              child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: Center(
+                      child: SizedBox(
+                    width: 500,
+                    child: Column(children: [
+                      Expanded(
+                          flex: 1,
+                          child: Text(character.name, style: nameStyle)),
+                      Expanded(
+                          flex: 4,
+                          child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 24),
+                              child: Column(children: [
+                                _FieldRow("Species", character.species),
+                                _FieldRow(
+                                    "Type",
+                                    (character.type.isNotEmpty
+                                        ? character.type
+                                        : "not specified")),
+                                _FieldRow("Origin", character.origin),
+                                const Text("Episodes",
+                                    style: episodesHeaderStyle),
+                                const SizedBox(height: 10),
+                                Expanded(
+                                    child: BlocBuilder<ContentLoaderBloc,
+                                        ContentLoaderState>(
+                                  bloc: episodesLoader,
+                                  builder: (context, state) {
+                                    if (state is ContentLoaded<List<Episode>>) {
+                                      List<Episode> episodes = state.content;
+                                      return ListView.builder(
                                           itemCount: episodes.length,
                                           itemBuilder: ((context, index) {
-                                        Episode episode = episodes[index];
-                                        return Row(children: [
-                                          Text(
-                                              "[${episode.code}] ${episode.name}")
-                                        ]);
-                                      })))
-                                    ]))),
-                          ]),
-                        ))))
-              ]);
-            } else {
-              throw Exception("Unknown state");
-            }
-          },
-        ));
+                                            Episode episode = episodes[index];
+                                            return Row(children: [
+                                              Text(
+                                                  "[${episode.code}] ${episode.name}")
+                                            ]);
+                                          }));
+                                    }
+                                    else if (state is NoContent) {
+                                      return Container();
+                                    }
+                                    else if (state is LoadingError){
+                                      return const Text('woops');
+                                    }
+                                    else {
+                                      throw Exception("Unknown state");
+                                    }
+                                  },
+                                ))
+                              ]))),
+                    ]),
+                  ))))
+        ]));
   }
 }
 
